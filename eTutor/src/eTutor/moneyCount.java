133be567ee5986cdfd277936690e62f6ed8dc4d6
hongyuh <m10316010@go.utaipei.edu.tw>
2015-03-23 02:11:28 +0800

    homework

---
+++ eTutor/src/eTutor/moneyCount.java
@@ -0,0 +1,26 @@
+package eTutor;
+
+/* e-tutor 分錢問題
+import java.util.Scanner;
+
+public class Main  
+{  
+public static void main(String[] args)  
+{   
+	Scanner sc = new Scanner(System.in);
+	int total=sc.nextInt();
+	
+	int ten= total/10;
+	int tenMod= total%10;
+	
+	int five=tenMod/5;
+	int fiveMod= tenMod%5;
+		
+	sc.close();
+	
+	System.out.println("NT10="+ten);
+	System.out.println("NT5="+five);
+	System.out.println("NT1="+fiveMod);
+	}  
+}
+*/
